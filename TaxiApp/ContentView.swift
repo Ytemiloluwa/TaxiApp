4d5e6081eb8f32967ddefb009623435a31ba0a7f
amtello <ytemilouwa@yahoo.com>
2020-04-25 14:05:11 +0100

    Initial Commit

---
+++ TaxiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaxiApp
+//
+//  Created by Tello on 25/04/2020.
+//  Copyright © 2020 Tello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
